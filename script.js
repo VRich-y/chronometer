d8bd0028652638764cc8fe5b7d2b0614113b1d6f
Richardy Bispo Vitorino <richardy.vitorino@acotubo.com.br>
2024-05-22 09:39:47 -0300

    Atualização

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
-var sec=00
-var min=00
-var hr=00
-var internal
+var sec = 0;
+var min = 0;
+var hr = 0;
+var interval;
+var isWorking = false;
 
-function doubleDigits(digit){
-    if(digit < 10){
-        return('0'+digit)
-    }else{
-        return(digit)
+function doubleDigits(digit) {
+    if (digit < 10) {
+        return '0' + digit;
+    } else {
+        return digit;
     }
 }
 
-function start(){
-    timer()
-    interval = setInterval(timer,1000)
-}
-function pause(){
-    clearInterval(interval)
-}
-function stop(){
-    clearInterval(interval)
-    sec=00
-    min=00
-    document.getElementById('timer').innerText='00:00:00'
-}
-function timer(){
-    sec=sec+1 //pode-se usar apenar sec++
-    if(sec==60){
-        min++
-        sec=00
-        if(min==60){
-            hr++
-            min=00
+function start() {
+    if (!isWorking) {
+        timer();
+        interval = setInterval(timer, 1000);
     }
-    
-    }
-    document.getElementById('timer').innerText=doubleDigits(hr)+':'+doubleDigits(min)+':'+doubleDigits(sec)
 }
 
+function pause() {
+    clearInterval(interval);
+    isWorking = false;
+}
+
+function stop() {
+    clearInterval(interval);
+    sec = 0;
+    min = 0;
+    hr = 0; 
+    document.getElementById('timer').innerText = '00:00:00';
+    isWorking = false;
+}
+
+function timer() {
+    sec++;
+    if (sec == 60) {
+        min++;
+        sec = 0;
+        if (min == 60) {
+            hr++;
+            min = 0;
+        }
+    }
+    document.getElementById('timer').innerText = doubleDigits(hr) + ':' + doubleDigits(min) + ':' + doubleDigits(sec);
+    isWorking = true;
+}
